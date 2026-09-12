e804f1e38c7a71df3f0a0edb53160d32f756365c
Maya Hossam <mayaarafaa@gmail.com>
2026-09-12 05:17:36 +0300

    Implement fees management

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -19,6 +19,7 @@ import BackOfficeManagement from "./pages/BackOfficeManagement";
 import NotFound from "./pages/NotFound";
 import { getUser, isAuthenticated } from "./lib/auth";
 import TransactionHistoryPage from "./pages/TransactionHistoryPage";
+import FeesManagement from "./pages/institution/FeesManagement";
 
 const InstitutionDashboard = lazy(() =>
   import("./pages/institution/InstitutionDashboard"),
@@ -63,7 +64,7 @@ export default function App() {
             }
           >
             <Route index element={<InstitutionDashboard />} />
-            <Route path="fees" element={<ComingSoon />} />
+            <Route path="fees" element={<FeesManagement />} />
             <Route path="discounts" element={<ComingSoon />} />
             <Route path="payments" element={<ComingSoon />} />
             <Route path="epp-plans" element={<ComingSoon />} />
